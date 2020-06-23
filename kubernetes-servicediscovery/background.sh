51cebf82e36b7af192bf8b8a67b26b88109a1cef
Ram Sateesh Talari <ramsateesh@gmail.com>
2020-06-23 10:54:10 +1000

    Service Discovery

--- kubernetes-servicediscovery/background.sh
+++ kubernetes-servicediscovery/background.sh
@@ -1,7 +1,3 @@
+#!/bin/bash
+
 mkdir -p /root/tutorials/k8s-service-discovery
-
-cd /root/tutorials/k8s-service-discovery/
-
-echo "Waiting for Kubernetes config to be setup."
-
-sleep 10
---
+++ kubernetes-servicediscovery/foreground.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /root/tutorials/k8s-service-discovery/
+
+echo "Waiting for Kubernetes config to be setup."
+
+sleep 10
